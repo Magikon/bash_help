41bc0d233923c752e0faad9b6023d41d264d3bad
Magikon <mikayel.galyan@gmail.com>
2022-12-03 17:56:30 +0400

    add instanse start/stop script
    ./instanceState.sh -n mik* -s stop -r us-west-1 -p someprofile

---
+++ instanceState.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+region=<region>
+profile=<profilename>
+state=start # or stop
+name=somename*
+while getopts p:r:n:s: flag
+do
+    case "${flag}" in
+        p) profile=${OPTARG};;
+        r) region=${OPTARG};;
+        n) name=${OPTARG};;
+        s) state=${OPTARG};;
+    esac
+done
+echo "-----------------";
+echo "profile: $profile";
+echo "region: $region";
+echo "name: $name";
+echo "state: $state";
+echo "-----------------";
+
+echo "Please enter code from your mfa device"
+aws-mfa --profile $profile # if you use aws-mfa, else comment this line
+declare -a IDs
+for i in $(aws ec2 describe-instances --filters "Name=tag:Name,Values=$name" --region $region --profile $profile --output text --query 'Reservations[*].Instances[*].InstanceId')
+do
+    IDs+=($i);
+    echo $i;
+done
+
+echo "Changing your instance state...."
+for c in ${!IDs[@]}; do
+    case $state in
+      stop)
+        aws ec2 stop-instances --instance-ids ${IDs[$c]} --region $region --profile $profile;;
+      start)
+        aws ec2 start-instances --instance-ids ${IDs[$c]} --region $region --profile $profile;;
+    esac
+done
+for c in ${!IDs[@]}; do
+   instancename=$(aws ec2 describe-instances --instance-ids ${IDs[$c]} --region $region --profile $profile --query 'Reservations[].Instances[].Tags[?Key==`Name`].Value' --output text)
+        while :; do
+            if [[ $state == "start" ]];then
+                temp=$(aws ec2 describe-instance-status --instance-ids ${IDs[$c]} --region $region --profile $profile --query 'InstanceStatuses[*].InstanceStatus[].Status' --output text)
+                if [[ $temp == "ok" ]];then break;fi
+                echo $instancename $temp;
+            fi
+            if [[ $state == "stop" ]];then
+                temp=$(aws ec2 describe-instances --instance-ids ${IDs[$c]} --region $region --profile $profile --output text --query 'Reservations[*].Instances[*].State[].Name');
+                if [[ $temp == "stopped" ]];then break;fi
+                echo $instancename $temp;
+            fi
+            sleep 5;
+        done
+   echo "instanceId = ${IDs[$c]}, $instancename  now in $temp state"
+done
+
+echo "Done"
